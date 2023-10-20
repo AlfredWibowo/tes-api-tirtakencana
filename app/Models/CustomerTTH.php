2c6649bb34cb2f070cc2acdbf08984f9b69476a1
AlfredWibowo <wibowoalfred@gmail.com>
2023-10-20 16:01:14 +0700

    update model

--- app/Models/CustomerTTH.php
+++ app/Models/CustomerTTH.php
@@ -10,10 +10,29 @@ class CustomerTTH extends Model
 {
     use HasFactory;
 
-    public function __construct(array $attributes = [])
-    {
-        parent::__construct($attributes);
+    // public function __construct(array $attributes = [])
+    // {
+    //     parent::__construct($attributes);
 
-        $this->table = DB::raw('`dbo.customertth`');
+    //     $this->table = DB::raw('`dbo.customertth`');
+    // }
+
+    protected $table = 'customertth';
+    protected $appends = ['CustomerTTHDetail'];
+    public $timestamps = false;
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class, 'CustID', 'CustID');
+    }
+
+    public function customerTTHDetail()
+    {
+        return $this->hasOne(CustomerTTHDetail::class, 'TTOTTPNo', 'TTOTTPNo');
+    }
+
+    public function getCustomerTTHDetailAttribute()
+    {
+        return $this->customerTTHDetail()->first();
     }
 }
